1b27d77ff3d45d398b67b0fc531fbdc3c48beafd
qluan <16888917+qluan1@users.noreply.github.com>
2025-01-06 19:38:10 -0800

    day 14 second half

--- solver/day14.go
+++ solver/day14.go
@@ -60,6 +60,66 @@ func d14FirstPart(input string) (string, error) {
 	return strconv.Itoa(q1*q2*q3*q4), nil
 }
 
+func d14CountConnectedComponents(robots [][4]int) int {
+	d := make(map[[2]int]int)	
+	for i, robot := range robots {
+		d[[2]int{robot[0], robot[1]}] = i
+	}
+	arr := make([]int, len(robots))
+	for i := range arr {
+		arr[i] = i
+	}
+	var find func(int) int
+	find = func(x int) int {
+		if arr[x] != x {
+			arr[x] = find(arr[x])
+		}
+		return arr[x]
+	}
+	union := func(x, y int) {
+		arr[find(x)] = find(y)
+	}
+	for i, robot := range robots {
+		x, y := robot[0], robot[1]
+		if _, ok := d[[2]int{x+1, y}]; ok {
+			union(i, d[[2]int{x+1, y}])
+		}
+		if _, ok := d[[2]int{x, y+1}]; ok {
+			union(i, d[[2]int{x, y+1}])
+		}
+	}
+	for i := range arr {
+		find(i)
+	}
+	components := make(map[int]bool)
+	for _, v := range arr {
+		components[v] = true
+	}
+	return len(components)
+}
+
 func d14SecondPart(input string) (string, error) {
-	return "", nil
+	robots := make([][4]int, 0)
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		x, y, vx, vy := readRobot(line)
+		robots = append(robots, [4]int{x, y, vx, vy})
+	}
+	wide, tall := 101, 103
+	step := -1
+	for i := 0; i < 100000; i++ {
+		connectedComponents := d14CountConnectedComponents(robots)
+		if connectedComponents < len(robots)/2 {
+			step = i	
+			break
+		}
+		for i, robot := range robots {
+			x, y := getNextPos(robot[0], robot[1], robot[2], robot[3], wide, tall)
+			robots[i] = [4]int{x, y, robot[2], robot[3]}
+		}
+	}
+	if step == -1 {
+		return "", fmt.Errorf("no solution found by the 100000th step")
+	}
+	return strconv.Itoa(step), nil
 }
